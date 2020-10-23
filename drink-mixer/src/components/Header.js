68a39731af5127fa57ee583e43ee8549ae3a4737
Brady Dawley <dawleybradym@johndeere.com>
2020-10-23 12:55:39 -0500

    Added links to header

--- drink-mixer/src/components/Header.js
+++ drink-mixer/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
             <h1>Drink Mixer</h1>
             <nav>
-                
+                <Link to='/random-drink'>Random Drink</Link>
+                <Link to='/by-spirit'>Drink By Spirit</Link>
+                <Link to='all-drinks'>All Drinks</Link>
+                <Link to='create-drink'>Create Drink</Link>
             </nav>
         </header>
     )
