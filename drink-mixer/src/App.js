431701d800732ce5075ce11d8a01ff7f6f5656be
Tim Kaiser <kaisertimothy@johndeere.com>
2020-10-23 15:15:15 -0500

    created all drink data

--- drink-mixer/src/App.js
+++ drink-mixer/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Route } from 'react-router-dom';
 
 import Header from './components/Header';
+import AllDrinks from './components/AllDrinks/AllDrinks';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <AllDrinks />
     </div>
   )
 }
