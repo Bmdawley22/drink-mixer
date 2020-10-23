40a9c5a3d559c0fb1d79c86b7f35c39cd171d993
Brady Dawley <dawleybradym@johndeere.com>
2020-10-23 14:56:13 -0500

    created BySpirit component, rendered categories, added route on app.js

--- drink-mixer/src/App.js
+++ drink-mixer/src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import { Route } from 'react-router-dom';
 
 import Header from './components/Header';
+import BySpirit from './components/BySpirit';
+
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Route path='/by-spirit' render={() => {
+        return <BySpirit />
+      }}
+      />
     </div>
   )
 }
 
 export default App;
+
