c6ba995dc80c4491901ccfffd8931f4d16f87a9e
Brady Dawley <dawleybradym@johndeere.com>
2020-10-23 12:50:48 -0500

    Initial app setup

---
+++ drink-mixer/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Drink Mixer</h1>
+            <nav>
+                
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
