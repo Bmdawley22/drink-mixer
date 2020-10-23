5ac4edc3eea7274d37690da8b70b4e080cef5937
ambonetto <bonettoandream@johndeere.com>
2020-10-23 14:57:14 -0500

    Display Drink name and image

--- drink-mixer/src/App.js
+++ drink-mixer/src/App.js
@@ -1,3 +1,6 @@
+// import React
+import React, {Component} from 'react';
+
 import './App.css';
 import { Route } from 'react-router-dom';
 
@@ -6,15 +9,49 @@ import Header from './components/Header';
 // importing Random Drink
 import RandomDrink from './components/RandomDrink';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Route path="/random-drink" render={() => {
-        return <RandomDrink />
-      }} />
-    </div>
-  )
+// import for API
+import axios from 'axios';
+
+// class base
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      randomDrink: "",
+      apiDataError: false
+    }
+  }
+
+  // Random Drink API call
+  async componentDidMount() {
+    const URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+    try {
+      const response = await axios(URL);
+      console.log(response);
+
+      this.setState({
+        randomDrink: response.data.drinks[0]
+      })
+    } catch (e) {
+      this.setState({
+        apiDataError: true
+      })
+    }
+
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <Header />
+        <Route path="/random-drink" render={() => {
+          return <RandomDrink randomDrink={this.state.randomDrink}/>
+        }} />
+      </div>
+    )
+  }
 }
 
 export default App;
--- drink-mixer/src/components/RandomDrink.js
+++ drink-mixer/src/components/RandomDrink.js
@@ -3,9 +3,11 @@ import React from 'react';
 
 // function base
 const RandomDrink = (props) => {
+    console.log(props)
     return (
          <h1>Random Drink Generator</h1>
-    )
+        
+        )
 }
 
 // export 
