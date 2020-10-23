d45ef96f286b755b2bcd5fbd364c99d8b1a4e183
ambonetto <bonettoandream@johndeere.com>
2020-10-23 14:13:08 -0500

    Initial Setup

--- drink-mixer/src/App.js
+++ drink-mixer/src/App.js
@@ -3,10 +3,16 @@ import { Route } from 'react-router-dom';
 
 import Header from './components/Header';
 
+// importing Random Drink
+import RandomDrink from './components/RandomDrink';
+
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Route path="/random-drink" render={() => {
+        return <RandomDrink />
+      }} />
     </div>
   )
 }
---
+++ drink-mixer/src/components/RandomDrink.js
@@ -0,0 +1,12 @@
+// import React 
+import React from 'react';
+
+// function base
+const RandomDrink = (props) => {
+    return (
+         <h1>Random Drink Generator</h1>
+    )
+}
+
+// export 
+export default RandomDrink;
