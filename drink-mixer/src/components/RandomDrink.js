b0c1b614b9db334060ca3af04661aa9140168af0
ambonetto <bonettoandream@johndeere.com>
2020-10-25 10:59:32 -0500

    Display measurements only with no null values

--- drink-mixer/src/components/RandomDrink.js
+++ drink-mixer/src/components/RandomDrink.js
@@ -3,14 +3,19 @@ import React from 'react';
 
 // function base
 const RandomDrink = (props) => {
-    console.log(props)
+    // creating a new array for just the ingredients in each random drink
     let ingredients = [];
     for (let i = 1; i < 16; i++) {
-        const temp=`strIngredient${i}`
-        ingredients.push(props.randomDrink[temp])
+        const temp = `strIngredient${i}`;
+        ingredients.push(props.randomDrink[temp]);
     }
 
-     console.log(ingredients);
+    // creating a new array for just the measurements in each random drink
+    let measurements = [];
+    for (let i = 1; i < 16; i++) {
+        const temp = `strMeasure${i}`;
+        measurements.push(props.randomDrink[temp]);
+    }
 
     return (
         <div>
@@ -18,20 +23,32 @@ const RandomDrink = (props) => {
             <div className="drinkWrapper">
                 <div className="drinkInfo">
                     <h2>{props.randomDrink.strDrink}</h2>
-                    <p>Ingredients:</p>
-                    {ingredients.map((ingredient, id) => {
-                        return (
-                            <div>
-                                {ingredient ? <li key={id}>{ingredient}</li> : null}
-                            </div>
-                        )
-                    })}
+                    <div className="ingredient-container">Ingredients:
+                        <div className="measurements">
+                            {measurements.map((measurement, id) => {
+                                return (
+                                    <div>
+                                        {measurement ? <li key={id}>{measurement}</li> : null}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="ingredient">
+                            {ingredients.map((ingredient, id) => {
+                                return (
+                                    <div>
+                                        {ingredient ? <li key={id}>{ingredient}</li> : null}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>   
                     <p>{props.randomDrink.strInstructions}</p>
                 </div>
                 <img src={props.randomDrink.strDrinkThumb} alt={props.randomDrink.strDrink}/>
             </div>
         </div>
-        )
+    )
 }
 
 // export 
