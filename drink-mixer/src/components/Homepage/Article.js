622aa888708154fe0a8910591a97b27805b81a38
ambonetto <bonettoandream@johndeere.com>
2020-10-28 13:57:21 -0500

    Updated Styling and Added Article Info

--- drink-mixer/src/components/Homepage/Article.js
+++ drink-mixer/src/components/Homepage/Article.js
@@ -5,16 +5,15 @@ import React from 'react';
 const Article = () => {
     return(
         <div className='homepageCard'>
-            <h1>Mixology 101</h1>
             <h1>The Science of Cocktail Making</h1>
-            <h4>Last modified: 22 June 2019</h4>
+            <h6>Last modified: 22 June 2019</h6>
             <img className='articleImg' src='https://static.bartendersbusiness.com/en/articles/images/w/800/BartendersBusiness-07222019053646000000-5d354b6e83195.jpg' alt='Cocktail Making' />
-            <p>Cocktail making might seem mixing a vodka and tonic, but it's much more than that.</p>
-            <p>Cocktail making is known to be a science, and not just because of its magical powers to 
+            <p className='articleInfo'>Cocktail making might seem mixing a vodka and tonic, but it's much more than that.</p>
+            <p className='articleInfo'>Cocktail making is known to be a science, and not just because of its magical powers to 
                 uplift moods all over. A good cocktail, when mixed well with the right amount of concoction 
                 and ice, can be the ultimate crowd-pleaser. However, you might lose a bit of clout if your 
                 cocktail isn’t made the right way.</p>
-            <a className='articleLink' href='https://bartendersbusiness.com/en/articles/menu-intel-1/the-science-of-cocktail-making-77.htm' targer='_blank'>Continue Reading</a>
+            <a className='articleLink' href='https://bartendersbusiness.com/en/articles/menu-intel-1/the-science-of-cocktail-making-77.htm' target='_blank'>Continue Reading</a>
         </div>
     )
 }
--- drink-mixer/src/components/Homepage/DrinkOfTheWeek.js
+++ drink-mixer/src/components/Homepage/DrinkOfTheWeek.js
@@ -9,11 +9,10 @@ import ratingStarHalf from '@iconify/icons-mdi/star-half';
 function DrinkOfTheWeek(props) {
     return(
         <div className='homepageCard'>
-            <h1>Drink of the Week:</h1>
             <img src={props.drink.strDrinkThumb} alt='showDrinkImg' className='showDrinkImg' />
             <h1>{props.drink.strDrink}</h1>
+            <h3>Editor's notes:</h3>
             <div>
-                <h3>Editor's notes</h3>
                 <Icon icon={ratingStar} />
                 <Icon icon={ratingStar} />
                 <Icon icon={ratingStar} />
@@ -21,7 +20,7 @@ function DrinkOfTheWeek(props) {
                 <Icon icon={ratingStarHalf} />
                 <h4>This one is a must for anyone that has not left their apartment in months!</h4>
             </div>
-            <Link to='/all-drinks/show-drink/5'>See how to make it!</Link>
+            <Link className='articleLink' to='/all-drinks/show-drink/5'>See how to make it!</Link>
         </div>
     )
 }
