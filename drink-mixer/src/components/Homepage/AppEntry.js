d4b5e7af8a779e487265ba0a881f7748d8c5356f
ambonetto <bonettoandream@johndeere.com>
2020-10-29 08:40:03 -0500

    Creating dropdown entry

--- drink-mixer/src/components/Homepage/AppEntry.js
+++ drink-mixer/src/components/Homepage/AppEntry.js
@@ -8,10 +8,10 @@ class AppEntry extends Component {
 
         this.state = {
             month: '',
-            date: '',
+            day: '',
             year: ''
         }
-    }
+    } 
 
     // change the state to check birthday
     dataEntry = (e) => {
@@ -29,24 +29,25 @@ class AppEntry extends Component {
                 <form onSubmit={(e) => this.props.dateEntryCheck(e, this.state)}>
                     <select>
                         <option selected disabled>Month</option>
-                        <option value="1">January</option>
-                        <option value="2">February</option>
-                        <option value="3">March</option>
-                        <option value="4">April</option>
-                        <option value="5">May</option>
-                        <option value="6">June</option>
-                        <option value="7">July</option>
-                        <option value="8">August</option>
-                        <option value="9">September</option>
-                        <option value="10">October</option>
-                        <option value="11">November</option>
-                        <option value="12">December</option>
+                        <option value='1'>January</option>
+                        <option value='2'>February</option>
+                        <option value='3'>March</option>
+                        <option value='4'>April</option>
+                        <option value='5'>May</option>
+                        <option value='6'>June</option>
+                        <option value='7'>July</option>
+                        <option value='8'>August</option>
+                        <option value='9'>September</option>
+                        <option value='10'>October</option>
+                        <option value='11'>November</option>
+                        <option value='12'>December</option>
                     </select>
                     <select>
                         <option selected disabled>Day</option>
                     </select>
                     <select>
                         <option selected disabled>Year</option>
+
                     </select>
                     <input type="submit" value="Enter Drink Mixer" />
                 </form>
